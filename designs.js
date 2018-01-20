3e3d00c651abd0a1b2d1f92c68162be81acc70c2
George <g.dzhincharadze@gmail.com>
2018-01-20 15:57:29 +0300

    Add files via upload

--- designs.js
+++ designs.js
@@ -5,6 +5,40 @@
 
 function makeGrid() {
 
-// Your code goes here!
+// defining variables
+
+var rowNumber = $('#input_height').val();
+var colNumber = $('#input_width').val();
+var table = $('#pixel_canvas');
+
+table.children().remove();
+
+// adding rows
+	for(var i = 0; i < rowNumber; i++) {
+		table.append("<tr></tr>");
+
+		//adding columns
+		for (var j = 0; j < colNumber; j++) {
+			table.children().last().append("<td></td>");
+		}
+	}
+
+// Event Listener table cell click
+	table.children().on('click', 'td', function() {
+		var color = $("input[type='color']").val();
+		$(this).attr('bgcolor', color);
+	});
+
+// Event Listener table cell doubleclick
+	table.children().on('dblclick', 'td', function() {
+		$(this).attr('bgcolor', 'transparent');
+	});
 
 }
+
+//Event listener for Submit button click
+
+$("input[type='submit']").click(function(e){
+	e.preventDefault();
+	makeGrid();
+});
